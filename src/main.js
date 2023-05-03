4e8913a7d0341db6daf75cf288f854c9b66c8345
urswilke <urs.wilke@gmail.com>
2023-05-03 16:50:03 +0200

    make basic plot of excel data

--- src/main.js
+++ src/main.js
@@ -5,35 +5,37 @@ import {read_xlsx} from './readExcel.js'
 import * as Plot from "@observablehq/plot";
 
 import sharedStyles from './styles.css?inline';
-const numbers = [Math.random(), Math.random()];
+
+async function plot_histogramm() {
+	const data = sheet_table.filter(x => x.RowSubtitle === "abs")
+	const numbers = [Math.random(), Math.random()];
+	rectYchart.chartOptions = {
+		// style: {
+		// 	color: "var(--plot-primary)",
+		// },
+		color: {
+			type: "categorical"
+		},
+		marks: [
+			Plot.barY(data, {x: "RowTitle", y: "Value", fill: "ColTitle"})
+		]
+	}
+	
+}
+
 
 const rectYchart = document.querySelector('#rect-plot');
 rectYchart.appStyles = sharedStyles;
-rectYchart.chartOptions = {
-	style: {
-		color: "var(--plot-primary)",
-	},
-	marks: [
-		Plot.rectY(numbers)
-	]	
-}
-
-const regenButton = document.querySelector("#regen")
-regenButton.onClick = async () => {
-	const numbers = [Math.random(), Math.random()];
-	rectYchart.chartOptions = {
-		style: {
-			color: "var(--plot-primary)",
-		},
-		marks: [
-			Plot.rectY(numbers)
-		]
-	}
-}
-
-
 const filePath = 'Mappe1.xlsx';
 const sheet = "Daten";
 
 const sheet_table = await read_xlsx(filePath, sheet);
+
+plot_histogramm()
+
+
+const regenButton = document.querySelector("#regen")
+regenButton.onClick = plot_histogramm;
+
+
 console.log(sheet_table);
