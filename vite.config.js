c6faf75519083a3cd719c09128a840d472f2abda
urswilke <urs.wilke@gmail.com>
2023-05-05 13:28:51 +0200

    remove stuff not needed (?)

--- vite.config.js
+++ vite.config.js
@@ -1,48 +1,9 @@
 import { defineConfig } from 'vite'
-import topLevelAwait from "vite-plugin-top-level-await";
-import webfontDownload from 'vite-plugin-webfont-dl';
-import urlResolve from 'rollup-plugin-url-resolve';
-import resolve from '@rollup/plugin-node-resolve';
-import commonjs from '@rollup/plugin-commonjs';
-import esbuild from 'rollup-plugin-esbuild';
-
-// https://vitejs.dev/config/
 export default defineConfig({
-	base: '/', // # MAKE SURE TO CHANGE to match final deployment
-  build: {
-		lib: {
-			entry: 'index.html',
-      // entry: 'src/main.js',
-			formats: [ 'es' ]
+    base: '/',  
+    esbuild: {
+        supported: {
+          'top-level-await': true //browsers can handle top-level-await features
+        },
     },
-		rollupOptions: {
-			plugins: [
-				urlResolve({
-					cacheManager: '.cache',
-					minify: true,
-				}),
-				resolve({ browser: true }),
-				commonjs(),
-				esbuild({
-					minify: true,
-					target: 'es2022', 
-				})
-			]
-		},
-	},
-	plugins: [
-		topLevelAwait({
-			promiseExportName: "__tla",
-			promiseImportName: i => `__tla_${i}`
-		}),
-		webfontDownload(),
-	],
-	server: {
-		headers: {
-		"Cache-Control": "no-cache; max-age=1",
-    "Cross-Origin-Embedder-Policy": "require-corp",
-		"Cross-Origin-Opener-Policy": "same-origin",
-		"Cross-Origin-Resource-Policy": "cross-origin",
-		}
-	}
 })
