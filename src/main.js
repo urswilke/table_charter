f815e5af2a07d3e0349993a6586d5464bd8d899d
urswilke <urs.wilke@gmail.com>
2023-05-09 23:06:31 +0200

    keep order of labels in table book in plot

--- src/main.js
+++ src/main.js
@@ -17,8 +17,15 @@ function plot_histogramm() {
 		// style: {
 		// 	color: "var(--plot-primary)",
 		// },
+		// use ordered sequence of unique values:
+		// https://observablehq.com/@ee2dev/sorting-with-plot-a-collection-of-plot-examples#cell-102
+		// https://stackoverflow.com/a/14438954
+		x: {
+			domain: [...new Set(data.map((x) => x.ColSubtitle))],
+		},
 		color: {
 			type: "categorical",
+			domain: [...new Set(data.map((x) => x.RowTitle))],
 			legend: true
 		},
 		marks: [
