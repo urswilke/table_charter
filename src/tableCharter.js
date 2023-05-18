bd6b41bc3ed839a36fdd6b7a789f20073fab13a4
urswilke <urs.wilke@gmail.com>
2023-05-18 18:20:34 +0200

    also update plot when clicking button

--- src/tableCharter.js
+++ src/tableCharter.js
@@ -2,6 +2,8 @@ import { LitElement, css, html, unsafeCSS } from 'lit'
 import { when } from 'lit/directives/when.js';
 import './ojs-plot.js'
 import './tableBookData.js'
+import { gen_plot_options } from './gen_plot_types.js'
+
 
 const inspect = true // set to true for some console.log msgs
 
@@ -24,7 +26,7 @@ export class TableCharter extends LitElement {
 	render() {
 		return html`
             <table-book-data @update-data="${this.update_plot_data}"></table-book-data>
-            <ojs-plot></ojs-plot>
+            <ojs-plot .chartOptions=${gen_plot_options(this.plot_data)}></ojs-plot>
 		`;
 
 	}
