f15d9e4ba8dabb963fe111f084c761d93256835c
urswilke <urs.wilke@gmail.com>
2023-11-10 23:43:35 +0100

    first try to implement the observable plot generation with a class and helper functions (but doesn't work yet)

--- src/gen_plot_types.js
+++ src/gen_plot_types.js
@@ -26,61 +26,104 @@ export function gen_plot_options(data) {
 
 // TODO: find better solution for ... [x.ColTitle1, x.ColTitle2].join('\n') ...
 function gen_plot_options_cat(data) {
-	const x_order = [...new Set(data.plot_data.map((x) => [x.ColTitle1, x.ColTitle2].join('\n')))];
-	const fill_order = [...new Set(data.plot_data.map((x) => x.RowTitle1))];
-	return {
-		// style: {
-		// 	color: "var(--plot-primary)",
-		// },
-		// use ordered sequence of unique values:
-		// https://observablehq.com/@ee2dev/sorting-with-plot-a-collection-of-plot-examples#cell-102
-		// https://stackoverflow.com/a/14438954
-		x: {
-			domain: x_order,
-			label: null
-		},
-		y: {
-			label: null,
-		},
-		color: {
-			type: data.choices.color_scale,
-			domain: fill_order,
-			legend: true
-		},
-		marks: [
-			Plot.barY(
-				data.plot_data, 
-				// https://talk.observablehq.com/t/how-to-display-text-in-each-level-of-a-stacked-bar-chart-made-with-plot/6510/2
-				Plot.groupX(
-					{y: "sum"},
-					{
-						x: (x) => ([x.ColTitle1, x.ColTitle2].join('\n')),
-						y: "Value", 
-						fill: "RowTitle1",
-						order: fill_order, 
-						// another way to add tooltips:
-						//  tip: true,
-					}
-				)
-			),
-			Plot.textY(
-				data.plot_data,
-				Plot.stackY(
-					Plot.groupX(
-						{ y: "sum", text: "first" },
-						{
-							x: (x) => ([x.ColTitle1, x.ColTitle2].join('\n')),
-							y: "Value",
-							z: "RowTitle1",
-							text: (x) => (x.Value == 0 ? null : x.Value.toFixed(0)),
-							order: fill_order
-						}
-					)
-				)
-			),
-		]
-	};
+	return new CatOptions(data).opts()
 }
+
+class CatOptions {
+	constructor(data) {
+		this.data = data;
+		this.x_order = [...new Set(data.plot_data.map((x) => [x.ColTitle1, x.ColTitle2].join('\n')))];
+		this.fill_order = [...new Set(data.plot_data.map((x) => x.RowTitle1))];
+		this.xy = data.choices.xy
+		this.yx = this.xy === "x" ? "y" : "x"
+		this.bar_opts = this.get_bar_options()
+		this.text_opts = this.get_text_options()
+	}
+	get_bar_options() {
+		const o1 = {}
+		o1[this.xy] = "sum"
+		const o2 = {
+			fill: "RowTitle1",
+			order: this.fill_order,
+			// another way to add tooltips:
+			tip: true,
+		}
+
+		o2[this.xy] = "Value"
+		o2[this.yx] = (x) => ([x.ColTitle1, x.ColTitle2].join('\n'))
+
+		const res = grouper(
+			o1,
+			o2,
+			this.xy
+		)
+		return res
+	}
+	get_text_options() {
+		const o2 = this.bar_opts
+		o2[this.xy]["text"] = "first"
+		o2[this.yx]["text"] = (x) => (x.Value == 0 ? null : x.Value.toFixed(0))
+		o2["z"] = o2["fill"];
+		delete o2["fill"];
+		delete o2["tip"];
+		return o2
+	}
+	opts() {
+		const x = {
+			// y: {
+			// 	label: null,
+			// },
+			color: {
+				type: this.data.choices.color_scale,
+				domain: this.fill_order,
+				legend: true
+			},
+			marks: [
+				bar_plotter(
+					this.data.plot_data,
+					// https://talk.observablehq.com/t/how-to-display-text-in-each-level-of-a-stacked-bar-chart-made-with-plot/6510/2
+					this.bar_opts,
+					this.xy
+				),
+				texter(
+					this.data.plot_data,
+					stacker(
+						this.text_opts,
+						this.xy
+					),
+					this.xy
+				),
+			]
+		};
+		x[this.yx] = ({
+			domain: this.x_order,
+			label: null
+		})
+		return x
+	}
+}
+function grouper(o1, o2, xy) {
+	return xy === "y" ?
+		Plot.groupX(o1, o2) :
+		Plot.groupY(o1, o2)
+}
+function texter(data, options, xy) {
+	return xy === "y" ?
+		Plot.textY.apply(null, [data, options]) :
+		Plot.textX.apply(null, [data, options])
+}
+function stacker(options, xy) {
+	return xy === "y" ?
+		Plot.stackY.apply(null, [options]) :
+		Plot.stackX.apply(null, [options])
+}
+function bar_plotter(data, options, xy) {
+	return xy === "y" ?
+		Plot.barY.apply(null, [data, options]) :
+		Plot.barX.apply(null, [data, options])
+}
+
+
 function gen_plot_options_mw(data) {
 	return {
 		marginLeft: 150,
