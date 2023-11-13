5ff1ef574539c6eeacbb59c295c8ffd8bc3c4b39
urswilke <urs.wilke@gmail.com>
2023-11-13 14:20:33 +0100

    refactor: - use the same helper functions when defining the options for cat & mw - fix bugs - add RowTitle2 to tooltips

--- src/gen_plot_types.js
+++ src/gen_plot_types.js
@@ -4,6 +4,7 @@ export function gen_plot_options(data) {
 	if (data.length === 0 || data.plot_data.length === 0) {
 		return {};
 	}
+	data["op"] = prep_options(data)
 	let tab_type = data.plot_data[0].TabType;
 	switch (tab_type) {
 		case "CAT":
@@ -24,8 +25,8 @@ export function gen_plot_options(data) {
 	}
 }
 
-function gen_plot_options_cat(data) {
-    const x2 = data.choices.xy
+function prep_options(data) {
+	const x2 = data.choices.xy
     const x1 = x2 === "x" ? "y" : "x"
 	const label_joiner = x1 === 'y' ? '\n' : ' '
 	const label_joiner2 = x2 === 'y' ? '\n' : ' '
@@ -33,43 +34,72 @@ function gen_plot_options_cat(data) {
     const row_lab_fun = (x) => [...new Set([x.RowTitle1, x.RowTitle2])].join(label_joiner2);
 	const color_order = [...new Set(data.plot_data.map(row_lab_fun))];
 	const x_order = [...new Set(data.plot_data.map(col_lab_fun))];
+	
+	const plot_opts = {}
+	plot_opts[x2] = col_lab_fun
+	plot_opts[x1] =  "Value"
+	const x1_opts = {
+		label: null,
+	}
+	const x2_opts = {
+        domain: x_order,
+        label: null
+    }
+
+	return {
+		x2,
+		x1,
+		label_joiner,
+		label_joiner2,
+		col_lab_fun,
+		row_lab_fun,
+		color_order,
+		x_order,
+		plot_opts,
+		x1_opts,
+		x2_opts,
+	}
+}
+
+function gen_plot_options_cat(data) {
+	const { 
+		x2: x2,
+		x1: x1,
+		// label_joiner: label_joiner,
+		// label_joiner2: label_joiner2,
+		// col_lab_fun: col_lab_fun,
+		row_lab_fun: row_lab_fun,
+		color_order: color_order,
+		x_order: x_order,
+		plot_opts: plot_opts,
+		x1_opts: x1_opts,
+		x2_opts: x2_opts,
+	} = data.op
+
 	const group_ 	= x1 === "y" ? Plot.groupX  : Plot.groupY
     const text_ 	= x1 === "y" ? Plot.textY   : Plot.textX
     const stack_ 	= x1 === "y" ? Plot.stackY  : Plot.stackX
     const bar_      = x1 === "y" ? Plot.barY 	: Plot.barX
     
+	const group_args1 = {text: "first"}
+	group_args1[x1] = "sum"
+	// const group_args2 = {...group_args1, text: "first"}
+	const group_args2_bar = {
+		...plot_opts,
+		fill: row_lab_fun,
+		order: color_order,
+		// tip: true,
+	}
+	const group_args2_text = {
+		...plot_opts,
+		text: (x) => (x.Value == 0 ? null : x.Value.toFixed(0)),
+		z: row_lab_fun,
+		order: color_order,
+		title: tooltip_fun
+	}
+	const bar_opts = group_(group_args1, group_args2_bar)
+	const text_opts = group_(group_args1, group_args2_text)
   
-    function get_bar_options() {
-        const o1 = {}
-        o1[x1] = "sum"
-        const o2 = {
-            fill: row_lab_fun,
-            order: color_order,
-            tip: true,
-        }
-    
-        o2[x1] = "Value"
-        o2[x2] = col_lab_fun
-    
-        const res = group_(
-            o1,
-            o2
-        )
-        return res
-    }
-    
-    function get_text_options() {
-        const o2 = get_bar_options(x1)
-        o2[x1]["text"] = "first"
-        o2[x2]["text"] = (x) => (x.Value == 0 ? null : x.Value.toFixed(0))
-        o2["z"] = o2["fill"];
-        delete o2["fill"];
-        delete o2["tip"];
-        return o2
-    }
-
-
-
 
     const res = {
 		marginLeft: x1 === "y" ? 40 : 120,
@@ -79,69 +109,44 @@ function gen_plot_options_cat(data) {
             legend: true
         },
         marks: [
-            bar_(
-                data.plot_data,
-                // https://talk.observablehq.com/t/how-to-display-text-in-each-level-of-a-stacked-bar-chart-made-with-plot/6510/2
-                get_bar_options()
-            ),
-            text_(
-                data.plot_data,
-                stack_(
-					get_text_options()
-				)
-            ),
+            bar_(data.plot_data, bar_opts),
+			// https://talk.observablehq.com/t/how-to-display-text-in-each-level-of-a-stacked-bar-chart-made-with-plot/6510/2
+            text_(data.plot_data, stack_(text_opts)),
 			// x1 === "y" ? Plot.axisX({textAnchor: "start"}) : null
         ]
     };
-    res[x2] = ({
-        domain: x_order,
-        label: null
-    })
-	res[x1] = {
-		label: null,
-	}
+    res[x2] = x2_opts
+	res[x1] = x1_opts
 	return res
 }
 
-
-
-
-
-
-
-
-
-
 function gen_plot_options_mw(data) {
-	const x1 = data.choices.xy
-    const x2 = x1 === "x" ? "y" : "x"
-	const label_joiner = x1 === 'x' ? '\n' : ' '
-	const col_lab_fun = (x) => [x.ColTitle1, x.ColTitle2].join(label_joiner)
-    const row_lab_fun = (x) => x.RowTitle1;
-	const color_order = [...new Set(data.plot_data.map(row_lab_fun))];
+	const { 
+		x2: x2,
+		x1: x1,
+		// label_joiner: label_joiner,
+		// label_joiner2: label_joiner2,
+		// col_lab_fun: col_lab_fun,
+		row_lab_fun: row_lab_fun,
+		color_order: color_order,
+		x_order: x_order,
+		plot_opts: plot_opts,
+		x1_opts: x1_opts,
+		x2_opts: x2_opts
+	} = data.op
 
 	let line_opts = {
+		...plot_opts,
 		stroke: row_lab_fun
 	}
-	line_opts[x2] = col_lab_fun
-	line_opts[x1] =  "Value"
-	let dot_opts1 = {
-		stroke: row_lab_fun
+
+	let dot_opts = {
+		...line_opts,
+		fill: row_lab_fun,
+		stroke: "transparent",
+		r: 7,
+		title: tooltip_fun
 	}
-	dot_opts1[x2] = col_lab_fun
-	dot_opts1[x1] =  "Value"
-	
-	let dot_opts2 = dot_opts1
-	dot_opts2["fill"] = row_lab_fun,
-	dot_opts2["stroke"] = "transparent",
-	dot_opts2["r"] = 7,
-	dot_opts2["title"] = (x) => [
-		`Q: ${x.TabTitle}`, 
-		`row: ${x.RowTitle1}`, 
-		`head: ${x.ColTitle1}`, 
-		`col: ${x.ColTitle2}`, 
-		`val: ${x.Value.toFixed(1)}`,
-	].join("\n")
 
 	
 	const res = {
@@ -153,18 +158,24 @@ function gen_plot_options_mw(data) {
 		},
 		marks: [
 			Plot.lineY(data.plot_data, line_opts),
-			Plot.dot(data.plot_data, dot_opts1),
-			Plot.dot(data.plot_data, dot_opts2),
+			Plot.dot(data.plot_data, dot_opts),
 			// x2 === "x" ? Plot.axisX({textAnchor: "start"}) : null
 		]
 	};
-	res[x1] = {
-		label: null,
-	} 
-	res[x2] = {
-		label: null,
-		domain: [...new Set(data.plot_data.map(col_lab_fun))],
-	}
+	res[x1] = x1_opts
+	res[x2] = x2_opts
 
 	return res
 }
+
+const tooltip_fun = (x) => [
+	`Q: ${x.TabTitle}`, 
+	`row1: ${x.RowTitle1}`, 
+	// only write row2 if differing from row1:
+	x.RowTitle1 === x.RowTitle2 ? 
+		null : 
+		`row2: ${x.RowTitle2}`, 
+	`head: ${x.ColTitle1}`, 
+	`col: ${x.ColTitle2}`, 
+	`val: ${x.Value.toFixed(1)}`,
+].join("\n")
