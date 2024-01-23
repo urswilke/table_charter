4a9306346e819a908d6e04e7d7800934254689e7
urswilke <urs.wilke@gmail.com>
2024-01-23 16:14:51 +0100

    move helper functions to utils.js

--- src/tableDataSelector.js
+++ src/tableDataSelector.js
@@ -1,7 +1,7 @@
 import { LitElement, css, html, unsafeCSS } from 'lit'
 import { when } from 'lit/directives/when.js';
 
-import { xlsx_to_json_array, distinct } from './utils.js'
+import { xlsx_to_json_array, distinct, gen_header_table, gen_row_table, filter_sel_headers, filter_sel_rows, gen_plot_type_string } from './utils.js'
 
 import './selectors/question_selector.js'
 import './selectors/multi_selector.js'
@@ -279,81 +279,3 @@ export class TableDataSelector extends LitElement {
 
 window.customElements.define('table-data-selector', TableDataSelector)
 
-function gen_header_table(data) {
-	const arr = distinct(
-		data,
-		// TODO: HeadNo is 2 for first 2 Heads => correct in crosstabser!
-		["ColNo", "HeadNo", "ColTitle1", "ColTitle2"]
-	);
-	const first_two_titles = [... new Set(arr.map(x => x.ColTitle1))].slice(0, 2);
-	return arr.map(p =>
-		first_two_titles.includes(p.ColTitle1)
-		? { ...p, selected: true }
-		: { ...p, selected: false }
-	);
-}
-
-function gen_row_table(data) {
-	const arr = distinct(data, ["RowContent", "RowTitle1"])
-	const row_contents = [... new Set(arr.map(x => x.RowContent))];
-	var types_to_take;
-	if (row_contents.includes("Detail")) {
-		types_to_take = ["Detail"]
-	} else if (row_contents.includes("Summary")) {
-		types_to_take = ["Summary"]
-	} else {
-		// setdiff:
-		types_to_take = row_contents.filter(x => !["Valid", "Total"].includes(x));
-	}
-	return arr.map(p =>
-		types_to_take.includes(p.RowContent)
-		? { ...p, selected: true }
-		: { ...p, selected: false }
-	);
-}
-
-function filter_sel_headers(data, header_table) {
-	const arr_sel = header_table.filter(x => x.selected);
-	const col_fun2 = x => x.ColTitle2 || x.ColTitle1
-	const col_fun1 = x => x.ColTitle1
-	const res = data.filter(x => 
-		[... new Set(arr_sel.map(col_fun2))].includes(col_fun2(x)) &
-		[... new Set(arr_sel.map(col_fun1))].includes(col_fun1(x))
-	);
-	return res;
-}
-function filter_sel_rows(data, header_table) {
-	const arr_sel = header_table.filter(x => x.selected);
-	const col_fun2 = x => x.RowTitle1
-	const col_fun1 = x => x.RowContent
-	const res = data.filter(x => 
-		[... new Set(arr_sel.map(col_fun2))].includes(col_fun2(x)) &
-		[... new Set(arr_sel.map(col_fun1))].includes(col_fun1(x))
-	);
-	return res;
-}
-
-function gen_plot_type_string(tab_sel_obj) {
-	let tab_type = tab_sel_obj.num_type_data[0].TabType;
-	if (
-		tab_type === "CAT" ||
-		// mw question that has a column TabDetails with the value "100percent" in the 1st row and percent values are selected:
-		(tab_type === "MW" & tab_sel_obj.num_type_data[0].TabDetails === "100percent" & tab_sel_obj.choices.row_type === "%")
-
-	) {
-		return "bar";
-	}
-	if (tab_type === "MCG") {
-		return "line";
-	}
-	if (tab_type === "MDG") {
-		return "line";
-	}
-	if (tab_type === "MW") {
-		return "line";
-	}
-	else {
-		alert("Table type " + tab_type + " not implemented.")
-	}	
-	
-}
