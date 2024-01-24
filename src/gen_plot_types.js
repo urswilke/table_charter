urswilke/table_charter

commit a4583f790d693d8110cd19fba8d1124d0b95804c

change wording

src/gen_plot_types.js
@@ -239,6 +239,6 @@ const color_scheme_discrete = new Map([
 ])
 
 export const color_schemes = {
-	"linear": color_scheme_ordinal,
+	"ordinal": color_scheme_ordinal,
 	"categorical": color_scheme_discrete,
 }

src/selectors/colorscale_selector.js
@@ -45,7 +45,7 @@ export class ColorscaleSelector extends LitElement {
     render() {
         return html`
         <div class= "subselect">
-            <label for="colorscale-selector">colorscale</label>
+            <label for="colorscale-selector">color scale</label>
             <select id="colorscale-selector" @change=${this._update_colorscale} ?disabled=${this.colorscale_disabled}>
                 ${this.all_colorscales.map((col) => html`
                     <option .selected=${this.chosen_colorscale === col}>
@@ -55,7 +55,7 @@ export class ColorscaleSelector extends LitElement {
             </select>
         </div>
         <div class= "subselect">
-            <label for="colorscheme-selector">colorscheme</label>
+            <label for="colorscheme-selector">color scheme</label>
             <select id="colorscheme-selector" @change=${this._update_colorscheme}>
                 ${this.all_colorschemes.map((col) => html`
                     <option .selected=${this.chosen_colorscheme === col}>

src/tableDataSelector.js
@@ -56,9 +56,9 @@ export class TableDataSelector extends LitElement {
 		this.choices.tab_nos = this.params.tab_titles[0].TabNo
 		this.params.row_type = ["%", "n"];
 		this.choices.row_type = this.params.row_type[0];
-		this.params.color_scale = ["categorical", "linear"];
+		this.params.color_scale = ["categorical", "ordinal"];
 		this.params.color_schemes = {
-			"linear": [...color_schemes.linear.keys()],
+			"ordinal": [...color_schemes.ordinal.keys()],
 			"categorical": [...color_schemes.categorical.keys()],
 		}
 		this.sel_question_data()
@@ -107,7 +107,7 @@ export class TableDataSelector extends LitElement {
 			n_numeric_rowtitles / df_row_tit_val.length >= 0.4 & 
 			[... new Set(this.params.row_table.filter(x => x.selected).map(x => x.RowContent))] == "Detail"
 		) {
-			this.color_scale = "linear"
+			this.color_scale = "ordinal"
 		} else {
 			this.color_scale = "categorical"
 		}

commit 3d2a8fea7beb5ac3b8c24c8111f965b1d31086bf

fix: bug introduced in a4583f790d693d8110cd19fba8d1124d0b95804c (observable plot expects type = "linear" not "ordinal")

src/gen_plot_types.js
@@ -96,7 +96,7 @@ function gen_bar_plot_options(data) {
     const res = {
 		marginLeft: x1 === "y" ? 40 : 120,
         color: {
-			type: data.color_scale,
+			type: data.color_scale === "ordinal" ? "linear" : data.color_scale,
 			scheme: color_schemes_maps[data.color_scale].get(data.color_scheme),
             domain: color_order,
             legend: true
@@ -145,7 +145,7 @@ function gen_line_plot_options(data) {
 	const res = {
 		marginLeft: x2 === "x" ? 40 : 160,
 		color: {
-			type: data.color_scale,
+			type: data.color_scale === "ordinal" ? "linear" : data.color_scale,
 			scheme: color_schemes_maps[data.color_scale].get(data.color_scheme),
 			domain: color_order,
 			legend: true