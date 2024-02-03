86cf3de1d2f75fcd2962004520f019c3809091be
urswilke <urs.wilke@gmail.com>
2024-02-04 00:39:08 +0100

    also test clicking again

--- src/test/table_charter.test.js
+++ src/test/table_charter.test.js
@@ -3,19 +3,26 @@ import { $, expect } from '@wdio/globals'
 
 import '../tableCharter.js'
 
-describe('Lit component testing', () => {
+render(
+    html`<table-charter></table-charter>`,
+    document.body
+)
+const button = await $('table-charter')
+    .shadow$("div.content")
+    .$("div.column1")
+    .$("table-data-selector")
+    .shadow$("div.parent")
+    .$("button")
+
+describe('Button "Show/hide advanced settings" testing', () => {
     it('should change text to "Hide advanced settings" on click', async () => {
-        render(
-            html`<table-charter></table-charter>`,
-            document.body
-        )
-        const button = await $('table-charter')
-            .shadow$("div.content")
-            .$("div.column1")
-            .$("table-data-selector")
-            .shadow$("div.parent")
-            .$("button")
+
         await button.click()
         await expect(button).toHaveText('Hide advanced settings')
+    })
+
+    it('should change text back to "Show advanced settings" on click', async () => {
+        await button.click()
+        await expect(button).toHaveText('Show advanced settings')
     })
 })
