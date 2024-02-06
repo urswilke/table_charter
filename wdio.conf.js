7f0f6a81768cbdef2a6f0aae2bb505c24bd05051
urswilke <urs.wilke@gmail.com>
2024-02-06 13:41:54 +0100

    hide a lot of the info given in the console when running the tests

--- wdio.conf.js
+++ wdio.conf.js
@@ -86,7 +86,7 @@ export const config = Object.assign({}, {
     // Define all options that are relevant for the WebdriverIO instance here
     //
     // Level of logging verbosity: trace | debug | info | warn | error | silent
-    logLevel: 'info',
+    logLevel: 'silent',
     //
     // Set specific log levels per logger
     // loggers:
