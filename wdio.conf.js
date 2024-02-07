d4058cdd3ce914f0809eed938a2f7ebad29c63cc
urswilke <urs.wilke@gmail.com>
2024-02-07 11:32:14 +0100

    rerun ci chrome job with logLevel = "info"

--- wdio.conf.js
+++ wdio.conf.js
@@ -91,7 +91,7 @@ export const config = Object.assign({}, {
     // Define all options that are relevant for the WebdriverIO instance here
     //
     // Level of logging verbosity: trace | debug | info | warn | error | silent
-    logLevel: 'silent',
+    logLevel: 'info',
     //
     // Set specific log levels per logger
     // loggers:
