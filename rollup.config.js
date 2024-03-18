e806859e16eba87ff35efea11a04472c1193bbc3
urswilke <urs.wilke@gmail.com>
2024-03-18 20:42:04 +0100

    try more stuff from https://github.com/jlengstorf/learn-rollup/

--- rollup.config.js
+++ rollup.config.js
@@ -3,12 +3,16 @@ import resolve from "rollup-plugin-node-resolve";
 import commonjs from "rollup-plugin-commonjs";
 
 export default {
-    input: "src/index.js",
-    output: {
-        name: "table_charter",
-        file: "dist/table_charter.es.js",
-        format: "es",
-    },
+    // input: "src/index.js",
+    // output: {
+    //     name: "table_charter",
+    //     file: "dist/table_charter.es.js",
+    //     format: "es",
+    // },
+    entry: "src/index.js",
+    dest: "build/main.min.js",
+    format: "iife",
+    sourceMap: "inline",
     plugins: [
         commonjs(),
         resolve({
