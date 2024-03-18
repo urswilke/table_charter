380f0543b63ff8c984fbcbb383ecdc4fec21f235
urswilke <urs.wilke@gmail.com>
2024-03-18 11:42:02 +0100

    - write the dynamic contents of the header - and add a reactive language attribute to the  table-charter html element (now also the tooltip text language is updated when the language is changed in the header select element)

--- src/tableCharter.js
+++ src/tableCharter.js
@@ -1,7 +1,6 @@
 import { LitElement, css, html } from "lit";
 import "./ojs-plot.js";
 import "./tableDataSelector.js";
-import client_data from "./client_data.json";
 import { registerTranslateConfig, use } from "lit-translate";
 
 // approach from here: https://github.com/andreasbm/lit-translate/issues/29#issuecomment-863270983
@@ -22,6 +21,7 @@ registerTranslateConfig({
 });
 export class TableCharter extends LitElement {
     static properties = {
+        language: { type: String },
         data: { type: Array },
         plot_data: { type: Array },
     };
@@ -30,31 +30,32 @@ export class TableCharter extends LitElement {
     // (see: https://github.com/andreasbm/lit-translate/blob/8f313900f4cea95aa8eca7e7409dcf8815d58df2/README.md#-wait-for-strings-to-be-loaded-before-displaying-the-component)
     constructor() {
         super();
-        this._browser_language = navigator.language.replace(/-.*/, "") || "en";
+        this.language = this.language || "en";
         this.hasLoadedStrings = false;
     }
     shouldUpdate(changedProperties) {
         return this.hasLoadedStrings && super.shouldUpdate(changedProperties);
     }
     async connectedCallback() {
-        await use(this._browser_language);
+        await use(this.language);
         this.hasLoadedStrings = true;
         super.connectedCallback();
+    }
+
+    set language(val) {
+        (async () => {
+            await use(val);
+        })();
+        this._language = val;
+    }
+    get language() {
+        return this._language;
     }
 
     update_plot_data(e) {
         this.plot_data = e.detail.data;
     }
-    update_lang() {
-        const lang = this.renderRoot?.querySelector(
-            "#lang-selector-select",
-        ).value;
-        use(lang);
-    }
 
-    // https://lit.dev/docs/composition/component-composition/#passing-data-up-and-down-the-tree
-    // good example here:
-    // https://stackoverflow.com/a/72402114
     render() {
         return html`
             <div class="content">
@@ -103,15 +104,3 @@ export class TableCharter extends LitElement {
 }
 
 window.customElements.define("table-charter", TableCharter);
-
-const date = new Intl.DateTimeFormat("de", {
-    year: "numeric",
-    month: "short",
-    day: "numeric",
-}).format(new Date());
-const logo =
-    client_data.client_logo_base64 !== ""
-        ? "data:image/png;base64, " + client_data.client_logo_base64
-        : client_data.client_logo_url;
-
-const languages_array = ["en", "de"];
