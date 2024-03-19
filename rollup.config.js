2cf2b2ea9b4ac41373a7f4fe6ac9cfd638220d6d
urswilke <urs.wilke@gmail.com>
2024-03-19 14:27:38 +0100

    let's try this (lit-translate depends on an older version of lit --> https://www.npmjs.com/package/rollup-plugin-peer-deps-external#motivation seems to be fulfilled...)

--- rollup.config.js
+++ rollup.config.js
@@ -2,6 +2,7 @@ import replace from "@rollup/plugin-replace";
 import json from "@rollup/plugin-json";
 import resolve from "@rollup/plugin-node-resolve";
 import commonjs from "@rollup/plugin-commonjs";
+import peerDepsExternal from "rollup-plugin-peer-deps-external";
 
 export default {
     input: "src/index.js",
@@ -12,16 +13,13 @@ export default {
         format: "es",
     },
     plugins: [
+        peerDepsExternal(),
         commonjs(),
         replace({
             preventAssignment: true,
             "process.env.NODE_ENV": JSON.stringify("development"),
         }),
-        resolve({
-            jsnext: true,
-            main: true,
-            browser: true,
-        }),
+        resolve(),
         json(),
     ],
 };
