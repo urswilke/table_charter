a2397c0719593a232b37d3fa7bee8a27911ccc8d
urswilke <urs.wilke@gmail.com>
2024-03-19 13:17:17 +0100

    version 1.0.15 was useless ... (tried to remove @rollup/plugin-replace... but in the bundled file there is: var errors = process.env.NODE_ENV !== "production" ? [ ... )

--- rollup.config.js
+++ rollup.config.js
@@ -1,5 +1,4 @@
 import replace from "@rollup/plugin-replace";
-
 import json from "@rollup/plugin-json";
 import resolve from "@rollup/plugin-node-resolve";
 import commonjs from "@rollup/plugin-commonjs";
