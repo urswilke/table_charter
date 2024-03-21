ff650a09bbf038cb78a94947e0d4d7b2f92e6df8
urswilke <urs.wilke@gmail.com>
2024-03-21 11:25:48 +0100

    remove filtering (do that outside of table_charter)

--- src/utils.js
+++ src/utils.js
@@ -99,9 +99,7 @@ export function gen_plot_type_string(tab_sel_obj) {
 }
 
 export function prepare_data(data_compressed) {
-    const data = decompress(data_compressed).filter((x) =>
-        ["Detail", "MStatistics", "Summary"].includes(x.RowContent),
-    );
+    const data = decompress(data_compressed);
     const unique_combis = distinct(data, ["QuestNo", "TabNo"]).map(
         (x) => x.QuestNo + "-" + x.TabNo,
     );
