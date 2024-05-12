d57617cd3a503343eacc76edaa99f36951a08f24
urswilke <urs.wilke@gmail.com>
2024-05-12 18:09:01 +0200

    add tooltips

--- src/ojs-plot.js
+++ src/ojs-plot.js
@@ -206,6 +206,7 @@ export class OJSPlot extends LitElement {
                 .classed("cat-label gap", (d) =>
                     d[0].startsWith(fantasy_string),
                 )
+                .attr("title", (d) => d[0])
                 .classed("cat-label", true)
                 .style(grid_end, (d) => "span " + d[1].length)
                 .append("span")
@@ -223,6 +224,7 @@ export class OJSPlot extends LitElement {
                 .append("div")
                 .classed("cat-label gap", (d) => d.includes(fantasy_string))
                 .classed("cat-label", true)
+                .attr("title", (d) => d)
                 .append("span")
                 .text(function (d) {
                     return d.replace(RegExp("^" + fantasy_string + ".*"), "");
