urswilke/table_charter

commit ada49dfab46ce73dc3d357e3da85fdbdb5ccdf88

move objects outside of QuestionsTable definition

src/questions_table.js
@@ -15,69 +15,6 @@ export class QuestionsTable extends LitElement {
             // id: x.i_tab,
             clone: "♲",
         }));
-        function duplicate_row(e, cell) {
-            const row = cell.getRow();
-            var i = table.getRowPosition(row) - 1;
-            const data = table.getData().map((x) => ({
-                ...x,
-                id_index: Number(String(x.id).split("_")[1] || "0"),
-            }));
-            // array of indices of all questions with this i_tab:
-            const all_indices = data
-                .filter((x) => x.i_tab === data[i].i_tab)
-                .map((x) => x.id_index);
-            const new_subindex = Math.max(...all_indices) + 1;
-            let this_row = cell.getData();
-            this_row = { ...this_row, id: this_row.i_tab + "_" + new_subindex };
-            const id = row.getData().id;
-            // https://github.com/olifolkerd/tabulator/issues/4034#issuecomment-1326211853
-            table.addData([this_row], false, id);
-            var new_data = table
-                .getData()
-                .map((x, i) => ({ ...x, i_tab_dyn: i }));
-            table.updateData(new_data);
-            dispatch_event(cell, "clone-question", {
-                table: new_data,
-                id,
-            });
-        }
-        function show_hide_question(e, cell) {
-            cell.setValue(!cell.getValue());
-            // avoid that all questions are hidden:
-            if (
-                cell
-                    .getTable()
-                    .getData()
-                    .map((x) => x.show)
-                    .every((x) => !x)
-            ) {
-                cell.setValue(true);
-                return;
-            }
-            var new_data = table.getData();
-            table.updateData(new_data);
-            dispatch_event(cell, "show-hide-question", {
-                table: new_data,
-                id: cell.getData().id,
-            });
-        }
-
-        function edit_text(cell) {
-            dispatch_event(cell, "edit-text", {
-                text: cell.getValue(),
-                id: cell.getData().i_tab_dyn,
-            });
-        }
-        function dispatch_event(cell, event_name, detail) {
-            const options = {
-                detail,
-                bubbles: true,
-                composed: true,
-            };
-            cell.getElement().dispatchEvent(
-                new CustomEvent(event_name, options),
-            );
-        }
 
         const table_def = {
             data: table_data,
@@ -86,41 +23,7 @@ export class QuestionsTable extends LitElement {
             // TODO: needs sth like a table.on("dataChanged"...) event to update plot correctly!
             history: true,
             headerSortClickElement: "icon",
-            columns: [
-                //Define Table Columns
-                {
-                    title: "i",
-                    field: "i_tab",
-                    width: 30,
-                    hozAlign: "right",
-                },
-                {
-                    title: "👁️",
-                    field: "show",
-                    width: 30,
-                    hozAlign: "center",
-                    formatter: "tickCross",
-                    cellClick: show_hide_question,
-                    cellTap: show_hide_question,
-                },
-                {
-                    title: "Question",
-                    field: "TabTitle",
-                    editor: "textarea",
-                    editorParams: {
-                        shiftEnterSubmit: true,
-                    },
-                    cellEdited: edit_text,
-                },
-                {
-                    title: "Clone",
-                    field: "clone",
-                    width: 20,
-                    hozAlign: "center",
-                    cellClick: duplicate_row,
-                    cellTap: duplicate_row,
-                },
-            ],
+            columns,
         };
         const table = new Tabulator(
             this.renderRoot?.querySelector("#questions-table"),
@@ -156,3 +59,99 @@ export class QuestionsTable extends LitElement {
     ];
 }
 customElements.define("questions-table", QuestionsTable);
+
+function duplicate_row(e, cell) {
+    const row = cell.getRow();
+    var i = table.getRowPosition(row) - 1;
+    const data = table.getData().map((x) => ({
+        ...x,
+        id_index: Number(String(x.id).split("_")[1] || "0"),
+    }));
+    // array of indices of all questions with this i_tab:
+    const all_indices = data
+        .filter((x) => x.i_tab === data[i].i_tab)
+        .map((x) => x.id_index);
+    const new_subindex = Math.max(...all_indices) + 1;
+    let this_row = cell.getData();
+    this_row = { ...this_row, id: this_row.i_tab + "_" + new_subindex };
+    const id = row.getData().id;
+    // https://github.com/olifolkerd/tabulator/issues/4034#issuecomment-1326211853
+    table.addData([this_row], false, id);
+    var new_data = table.getData().map((x, i) => ({ ...x, i_tab_dyn: i }));
+    table.updateData(new_data);
+    dispatch_event(cell, "clone-question", {
+        table: new_data,
+        id,
+    });
+}
+function show_hide_question(e, cell) {
+    cell.setValue(!cell.getValue());
+    // avoid that all questions are hidden:
+    if (
+        cell
+            .getTable()
+            .getData()
+            .map((x) => x.show)
+            .every((x) => !x)
+    ) {
+        cell.setValue(true);
+        return;
+    }
+    var new_data = table.getData();
+    table.updateData(new_data);
+    dispatch_event(cell, "show-hide-question", {
+        table: new_data,
+        id: cell.getData().id,
+    });
+}
+
+function edit_text(cell) {
+    dispatch_event(cell, "edit-text", {
+        text: cell.getValue(),
+        id: cell.getData().i_tab_dyn,
+    });
+}
+function dispatch_event(cell, event_name, detail) {
+    const options = {
+        detail,
+        bubbles: true,
+        composed: true,
+    };
+    cell.getElement().dispatchEvent(new CustomEvent(event_name, options));
+}
+
+const columns = [
+    //Define Table Columns
+    {
+        title: "i",
+        field: "i_tab",
+        width: 30,
+        hozAlign: "right",
+    },
+    {
+        title: "👁️",
+        field: "show",
+        width: 30,
+        hozAlign: "center",
+        formatter: "tickCross",
+        cellClick: show_hide_question,
+        cellTap: show_hide_question,
+    },
+    {
+        title: "Question",
+        field: "TabTitle",
+        editor: "textarea",
+        editorParams: {
+            shiftEnterSubmit: true,
+        },
+        cellEdited: edit_text,
+    },
+    {
+        title: "Clone",
+        field: "clone",
+        width: 20,
+        hozAlign: "center",
+        cellClick: duplicate_row,
+        cellTap: duplicate_row,
+    },
+];

commit ee15b03dbf00487a6f264f047e60987fbb5b49d3

fix bugs introduced in ada49dfab46ce73dc3d357e3da85fdbdb5ccdf88

src/questions_table.js
@@ -56,6 +56,7 @@ export class QuestionsTable extends LitElement {
 customElements.define("questions-table", QuestionsTable);
 
 function duplicate_row(e, cell) {
+    const table = cell.getTable();
     const row = cell.getRow();
     var i = table.getRowPosition(row) - 1;
     const data = table.getData().map((x) => ({
@@ -92,6 +93,7 @@ function show_hide_question(e, cell) {
         cell.setValue(true);
         return;
     }
+    const table = cell.getTable();
     var new_data = table.getData();
     table.updateData(new_data);
     dispatch_event(cell, "show-hide-question", {